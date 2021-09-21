12220d4dc221de1a270bb5adc4c9d6a8aede6238
sarfarazshekh <sarfarazali.shekh@gmail.com>
2021-09-21 16:02:18 +0530

    Revert "Revert "Add project files.""
    This reverts commit e8f41e47f89639fb1da9e53ac71b2d34ace7ea79.

---
+++ RabbitMQ.Producer/HeaderExchangeProducer.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace RabbitMQ.Producer
+{
+    public static class HeaderExchangeProducer
+    {
+        public static void Publish(IModel chanel)
+        {
+            var ttl = new Dictionary<string, object>
+            {
+                {"x-message-ttl",30000 }
+            };
+            chanel.ExchangeDeclare("demo-header-exchange", ExchangeType.Topic, arguments: ttl);
+            int count = 0;
+            while (true)
+            {
+                var message = new { name = "Producer", message = $"Hello! count: {count}" };
+                var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+                var properties = chanel.CreateBasicProperties();
+                  properties.Headers  = new Dictionary<string, object> { { "account", "new" } };
+                chanel.BasicPublish("demo-header-exchange", string.Empty, properties, body);
+                count++;
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ RabbitMQ.Producer/QueueProducer.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace RabbitMQ.Producer
+{
+    public static class QueueProducer
+    {
+        public static void publish(IModel chanel)
+        {
+            chanel.QueueDeclare("demo-queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+            int count = 0;
+            while (true)
+            {
+                var message = new { name = "Producer", message = $"Hello! count: {count}" };
+                var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+                chanel.BasicPublish("", "demo-queue", null, body);
+                count++;
+                Thread.Sleep(1000);
+            }
+            
+
+        }
+    }
+}
